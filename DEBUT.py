42d8882849a8ab617ada01caba073e298a546ed3
Héléna Chevalier <lna.chevalier@gmail.com>
2025-01-02 20:10:29 +0100

    Variants en dict début

--- DEBUT.py
+++ DEBUT.py
@@ -14,28 +14,50 @@ def debut():
     couleurboutonsVar = 'snow'
     policevar = 'Franklin Gothic Medium Cond'
     lstcolo=['red','blue','green','cyan','pink','purple','yellow','orange']
-    lstbords=['black','lime green']
     maxcolo = len(lstcolo)
 
-    #Variants par défauts
-    variant_sablier = False
-    variant_score = False
-    variant_taille_boule = False
-    variant_dynamique = False
-    variant_terminaison = False
-    variant_obstacles = False
+    variants = {
+        "sablier": {
+            "x": 765,
+            "y": 350,
+            "actif": False,
+            "description": "Attention, posez vite votre boule !\nLe temps vous est compté avant que\nvotre tour soit passé !"
+        },
+        "score": {
+            "x": 1150,
+            "y": 350,
+            "actif": False,
+            "description": "Vous pouvez appuyer sur votre touche \"s\"\ndu clavier pour faire apparaître vos scores\npendant 2 secondes"
+        },
+        "choix-taille-boule": {
+            "x": 1535,
+            "y": 350,
+            "actif": False,
+            "description": "Vous décidez de la taille de votre boule pour\nle tour. Vous commencez avec un certain\nbudget fixé, pour chaque boule posée, votre\nbudget diminue."
+        },
+        "dynamique": {
+            "x": 765,
+            "y": 675,
+            "actif": False,
+            "description": "La taille évolue automatiquement au fur et\nà mesure que la partie avance"
+        },
+        "terminaison": {
+            "x": 1150,
+            "y": 675,
+            "actif": False,
+            "description": "Une fois dans la partie vous pouvez\ndécider de la faire se finir dans 5 tours"
+        },
+        "obstacles": {
+            "x": 1535,
+            "y": 675,
+            "actif": False,
+            "description": "Le tableau commence avec certains obstacles\nque vos boules ne peuvent pas toucher."
+        }
+    }
 
-    #Bords des Variants
-    bord_sablier = 0
-    bord_score = 0
-    bord_taille_boule = 0
-    bord_dynamique = 0
-    bord_terminaison = 0
-    bord_obstacle = 0
     a = 0 #EasterEgg
     min_tour, max_tour = 5, 50
     tour = min_tour
-
 
     #====================================GROS BOUTONS========================================================================================================================
 
@@ -77,43 +99,15 @@ def debut():
             #Longueur dediée aux variants : 1220 de largeur 
             # On va dire 65 px entre chaques variants  tailleX des variants : 320px
             # Ligne de séparation faire 550 de haut donc 50 px entre chaque et tailleY des variants : 200px
+            # longeur largeur 320  -  275 px 
 
-            #SABLIER
-            rectangle(765,350,1085,625,lstbords[bord_sablier],remplissage=couleurboutonsVar,epaisseur=10)
-            texte(925,375,"Sablier",ancrage='center',police=policevar,taille=20)
-            image(925,475, path.join('.','icones',"icone-sablier-couleur-reduit.png"),'center')
-            texte(925,585,"Attention, posez vite votre boule !\nLe temps vous est compté avant que\nvotre tour soit passé !",taille=10,ancrage='center',police=policevar)
-
-            #SCORES
-            rectangle(1150,350,1470,625,lstbords[bord_score],remplissage=couleurboutonsVar,epaisseur=10)
-            texte(1310,375,"Affichage des scores",ancrage='center',police=policevar,taille=20)
-            image(1310,475, path.join('.','icones',"icone-score-couleur-reduit.png"),'center')
-            texte(1310,585,"Vous pouvez appuyer sur votre touche \"s\"\ndu clavier pour faire apparaître vos scores\npendant 2 secondes",taille=10,ancrage='center',police=policevar)
-
-            #TAILLE
-            rectangle(1535,350,1855,625,lstbords[bord_taille_boule],remplissage=couleurboutonsVar,epaisseur=10)
-            texte(1695,375,"Choix taille des boules",ancrage='center',police=policevar,taille=20)
-            image(1695,475, path.join('.','icones',"icone-choix-taille-boule-couleur-reduit.png"),'center')
-            texte(1695,585,"Vous décidez de la taille de votre boule pour\n le tour. Vous commencez avec un certain\nbudget fixé, pour chaque boule posée, votre\nbudget diminue.",taille=10,ancrage='center',police=policevar)
-
-            #DYNAMIQUE
-            rectangle(765,675,1085,950,lstbords[bord_dynamique],remplissage=couleurboutonsVar,epaisseur=10)
-            texte(925,700,"Dynamique",ancrage='center',police=policevar,taille=20)
-            image(925,800, path.join('.','icones',"icone-dynamique-couleur-reduit.png"),'center')
-            texte(925,910,"La taille évolue automatiquement au fur et\nà mesure que la partie avance",taille=10,ancrage='center',police=policevar)
-
-            #TERMINAISON
-            rectangle(1150,675,1470,950,lstbords[bord_terminaison],remplissage=couleurboutonsVar,epaisseur=10)
-            texte(1310,700,"Terminaison",ancrage='center',police=policevar,taille=20)
-            image(1310,800, path.join('.','icones',"icone-terminaison-couleur-reduit.png"),'center')
-            texte(1310,910,"Une fois dans la partie vous pouvez\ndécider de la faire se finir dans 5 tours",taille=10,ancrage='center',police=policevar)
-
-            #OBSTACLES
-            rectangle(1535,675,1855,950,lstbords[bord_obstacle],remplissage=couleurboutonsVar,epaisseur=10)
-            texte(1695,700,"Obstacles",ancrage='center',police=policevar,taille=20)
-            image(1695,800, path.join('.','icones',"icone-obstacles-couleur-reduit.png"),'center')
-            texte(1695,910,"Le tableau commence avec certains obstacles\nque vos boules ne peuvent pas toucher.",taille=10,ancrage='center',police=policevar)
-
+            for key, item in variants.items():
+                bord = 'lime green' if item["actif"] else 'black'
+                x, y = item['x'], item['y']    
+                rectangle(x,y,x+320,y+275,bord,remplissage=couleurboutonsVar,epaisseur=10)
+                texte(x+160,y+25,key,ancrage='center',police=policevar,taille=20)
+                image(x+160,y+125, path.join('.','icones',f"icone-{key}-couleur-reduit.png"),'center')
+                texte(x+160,y+235,item['description'],taille=10,ancrage='center',police=policevar)
 
             #=======================================================LES CONDITIONNELLES=======================================================================
             rectangle(65,50,565,120, 'black', 'SandyBrown', epaisseur=2)
@@ -157,65 +151,17 @@ def debut():
                         a = 0
                     
                     #=================================================VARIANTS===========================================================
-                    #COORDONNEE VARIANT DYNAMIQUE rectangle 765,675,1085,950
-                    if 765<x<1085 and 675<y<950:
-                        if variant_dynamique == 0 :
-                            variant_dynamique = True
-                            bord_dynamique = 1
-                        elif bord_dynamique == 1 :
-                            variant_dynamique = False
-                            bord_dynamique = 0
-
-                    #COORDONNEE VARIANT SCORE rectangle 1150,350,1470,625
-                    if 1150<x<1470 and 350<y<625:
-                        if variant_score == 0 :
-                            variant_score = True
-                            bord_score = 1
-                        elif bord_score == 1 :
-                            variant_score = False
-                            bord_score = 0
-
-                    #COORDONNEE VARIANT CHOIX TAILLE rectangle 1535,350,1855,625
-                    if 1535<x<1855 and 350<y<625:
-                        if variant_taille_boule == 0 :
-                            variant_taille_boule = True
-                            bord_taille_boule = 1
-                        elif bord_taille_boule == 1 :
-                            variant_taille_boule = False
-                            bord_taille_boule = 0
-                
-                    #COORDONNEE VARIANT OBSTACLE rectangle 765,350,1085,625
-                    if 765<x<1085 and 350<y<625:
-                        if variant_sablier == 0 :
-                            variant_sablier= True
-                            bord_sablier = 1
-                        elif bord_sablier == 1 :
-                            variant_sablier = False
-                            bord_sablier = 0
-
-                    #COORDONNEE VARIANT TERMINAISON rectangle 1150,675,1470,950
-                    if 1150<x<1470 and 675<y<950:
-                        if variant_terminaison == 0 :
-                            variant_terminaison = True
-                            bord_terminaison = 1
-                        elif bord_terminaison == 1 :
-                            variant_terminaison = False
-                            bord_terminaison = 0
-
-                    #COORDONNEE VARIANT OBSTACLE rectangle 1535,675,1855,950
-                    if 1535<x<1855 and 675<y<950:
-                        if variant_obstacles == 0 :
-                            variant_obstacles = True
-                            bord_obstacle = 1
-                        elif bord_obstacle == 1 :
-                            variant_obstacles = False
-                            bord_obstacle = 0
+                    for key, item in variants.items():
+                        Ox, Oy = item['x'], item['y']  
+                        if Ox < x < (Ox + 320) and Oy < y < (Oy + 275):
+                            # Alterner l'état de "actif"
+                            item["actif"] = not item["actif"]
 
                     #=================================================BOUTONS PRINCIPAUX==============================================
                     #COORDONNEE DU BOUTON JOUER
                     if 65<x<635 and 250<y<366:
-                        lstvar = [variant_sablier,variant_score,variant_taille_boule,variant_dynamique,variant_terminaison,variant_obstacles]
                         efface_tout()
+                        lstvar = [data["actif"] for data in variants.values()]
                         return lstvar, tour
 
                     #COORDONNEE DU BOUTON QUITTER 
@@ -225,14 +171,14 @@ def debut():
 
                     #Boutons supplémentaires
                     if 65 < x < 565 and 50 < y < 120:
-                        lstvar = [variant_sablier,variant_score,variant_taille_boule,variant_dynamique,variant_terminaison,variant_obstacles]
                         efface_tout()
                         nom = str(enregistrement())
                         lien = path.join(".","saves_variantes",nom+'.txt')
+                        lstvar = [data["actif"] for data in variants.values()]
                         with open(lien,'x') as fichier :
                             fichier.write(str(lstvar)+'\n')
                             fichier.write(str(tour)+'\n')
-                        return lstvar, tour
+                        return variants, tour
 
 
                     if 65 <x <565 and 130 <y< 200:
@@ -242,6 +188,7 @@ def debut():
                             listes_fichiers.append(i)
                         fichiersa = str(choixsave(listes_fichiers))
                         lien = path.join('.','saves_variantes',fichiersa)
+                        lstvar = [data["actif"] for data in variants.values()]
                         with open(lien, 'r') as liste :
                             lstvar = eval(liste.readline()[:-1])
                             tour = int(liste.readline()[:-1])
